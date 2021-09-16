3a2752474de2366a83ad7cea0b44223cb58eb5d8
Shubham Rajput <rajput.shubh1993@gmail.com>
2021-09-16 21:49:20 +0530

    Add edit user functionality

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
 
+  
+  protected
+
+    def update_resource(resource, params)
+      resource.update_without_password(params)
+    end
+
   private
 
     def configure_permitted_parameters
