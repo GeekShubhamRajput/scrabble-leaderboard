b100277f51658231a7871b975e722fe758d24a86
Shubham Rajput <rajput.shubh1993@gmail.com>
2021-09-16 19:09:44 +0530

    Create landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  get 'home/index'
+  root to: "home#index"
+
   devise_for :users, controllers: { registrations: "users/registrations" }
+
+  resources :clubs
+  
 end
